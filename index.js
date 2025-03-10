da974e9ba93a6993a0fb6ffd4f6749457bb47675
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-03-10 22:32:33 +0300

    implements functions to convert array to Title Case

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+//Plan: use the map() mathod to create a new array of transformed strings
+//for each tutorial title: split it into words, Capitalize the first letter of each word
+//Join the words back together
+//Return the new array
+
+
+function titleCased() {
+  return tutorials.map(function (tutorial) { //loops through each tutorial title
+    return tutorial
+      .split(" ") // converts the title into an array of words.
+      .map(function (word) { //loops through each word
+        return word.charAt(0).toUpperCase() + word.slice(1); //Capitalizes the first letter & Keeps the rest of the word unchanged
+      }) 
+      .join(" "); // Join words back into a sentence
+  });
 }
+
+console.log(titleCased());
+
+
+
+
+
+
+// const titleCased = () => {
+//   return tutorials
+// }
